b436e7ffe35ec3b7afb20e321d61435174ae42b2
Brozda <daniel.brozik@gmail.com>
2025-10-06 10:57:15 +0200

    Feat: Migration to change sensor value type

---
+++ WeatherMonitorReader.Infrastructure/Migrations/20251005211514_ChangeSensorValueToString.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeatherMonitorReader.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeSensorValueToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Value",
+                table: "WeatherMonitorSensorReadings",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Value",
+                table: "WeatherMonitorSensorReadings",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
